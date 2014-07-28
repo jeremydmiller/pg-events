00a38827235f118d79a658b89aa8eb4165e220f8
jeremydmiller <jeremydmiller@yahoo.com>
2014-07-28 14:45:36 -0500

    Calling the view storage warmup in initialization

--- lib/sql/initializer.sql
+++ lib/sql/initializer.sql
@@ -52,7 +52,7 @@ for (var i = 0; i < results.length; i++){
 	eval(results[i].definition);
 }
 
-persistor.buildProjectionStores(projector.views);
+persistor.warmupViewStores(projector.views);
 
 plv8.projector = projector;
 plv8.store = persistor;
